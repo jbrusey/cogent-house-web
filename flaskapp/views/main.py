d4ec193217bb8d77d05b2c24648968cbf4520f33
James Brusey <james.brusey@gmail.com>
2025-08-08 09:57:32 +0800

    convert missing page

--- flaskapp/views/main.py
+++ flaskapp/views/main.py
@@ -1,6 +1,16 @@
-from flask import Blueprint, render_template
+from datetime import UTC, datetime, timedelta
 
-from cogent.base.model import Node, Session
+from flask import Blueprint, render_template
+from sqlalchemy import distinct, func
+
+from cogent.base.model import (
+    House,
+    Location,
+    Node,
+    NodeState,
+    Room,
+    Session,
+)
 
 main_bp = Blueprint("main", __name__)
 
@@ -18,3 +28,73 @@ def nodes():
     finally:
         session.close()
     return render_template("nodes.html", title="Nodes", nodes=records)
+
+
+@main_bp.route("/missing")
+def missing():
+    """Report nodes missing in the last eight hours and extra nodes."""
+    t = datetime.now(UTC) - timedelta(hours=8)
+    session = Session()
+    try:
+        report_set = {
+            int(x)
+            for (x,) in session.query(distinct(NodeState.nodeId))
+            .filter(NodeState.time > t)
+            .all()
+        }
+        all_set = {
+            int(x)
+            for (x,) in session.query(Node.id)
+            .join(Location, Node.locationId == Location.id)
+            .join(House, Location.houseId == House.id)
+            .join(Room, Location.roomId == Room.id)
+            .all()
+        }
+        missing_set = all_set - report_set
+        extra_set = report_set - all_set
+
+        missing_nodes = []
+        if missing_set:
+            qry = (
+                session.query(
+                    func.max(NodeState.time),
+                    NodeState.nodeId,
+                    House.address,
+                    Room.name,
+                )
+                .filter(NodeState.nodeId.in_(missing_set))
+                .group_by(NodeState.nodeId)
+                .join(Node, NodeState.nodeId == Node.id)
+                .join(Location, Node.locationId == Location.id)
+                .join(House, Location.houseId == House.id)
+                .join(Room, Location.roomId == Room.id)
+                .order_by(House.address, Room.name)
+                .all()
+            )
+            for maxtime, node_id, house, room in qry:
+                missing_nodes.append(
+                    {
+                        "node": node_id,
+                        "house": house,
+                        "room": room,
+                        "last": maxtime,
+                        "unregister_url": f"/unregisterNode?node={node_id}",
+                    }
+                )
+
+        extra_nodes = []
+        if extra_set:
+            for node_id in sorted(extra_set):
+                extra_nodes.append(
+                    {
+                        "node": node_id,
+                        "register_url": f"/registerNode?node={node_id}",
+                    }
+                )
+
+    finally:
+        session.close()
+
+    return render_template(
+        "missing.html", title="Missing nodes", missing=missing_nodes, extra=extra_nodes
+    )
