b824811b49b03f9517ad54f6aeaa765d3539dbb5
James Brusey <james.brusey@gmail.com>
2025-09-02 08:43:03 +0100

    use SCRIPT_NAME to control prefix

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,7 +1,8 @@
 from . import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
-app.config["APPLICATION_ROOT"] = "/silicon"
+app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1)  # trust proxy headers
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
